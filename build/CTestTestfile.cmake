e7508f2e3a338fa114b5afa0c1866b8555ecc320
Florian Quibel <florian.quibel27@gmail.com>
2018-11-08 13:06:07 +0100

    update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/florian/Documents/dvb_ws/ROS_Libraries/src
+# Build directory: /home/florian/Documents/dvb_ws/ROS_Libraries/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(dvb_hardware)
+subdirs(dvb_robot)
+subdirs(dvb_map)
---
+++ build/dvb_hardware/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/florian/Documents/dvb_ws/ROS_Libraries/src/dvb_hardware
+# Build directory: /home/florian/Documents/dvb_ws/ROS_Libraries/build/dvb_hardware
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
